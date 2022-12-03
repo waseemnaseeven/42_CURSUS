01625bfcd12c46236895f1225fbe181deb50e24a
Waseem NASEEVEN <wnaseeve@e1r5p14.clusters.42paris.fr>
2022-12-03 17:45:18 +0100

    add examrank2 exercises

---
+++ EXAM_RANK02/last_word.c
@@ -0,0 +1,25 @@
+int	main(int argc, char **argv)
+{
+	int  i = 0;
+	if (argc == 2)
+	{
+		while (argv[1][i])
+			i++;
+		i--;
+		while (argv[1][i] == ' ' || argv[1][i] == '\t')
+			i--;
+		while (i > 0)
+		{
+			if (argv[1][i] == ' ' || argv[1][i] == '\t')
+				break;
+			i--; 
+		}
+		i++;
+		while (argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t')
+		{
+			write(1, &argv[1][i], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+}
